ca0baf55d7d12758a38515dd4d4ccd0304e52ded
柯瑞霖 <113598040@cc.ntut.edu.tw>
2025-09-26 17:02:04 +0800

    docx(reset): add reset.ps1

---
+++ reset.ps1
@@ -0,0 +1,3 @@
+Remove-Item -Recurse log/* -ErrorAction SilentlyContinue
+Remove-Item -Recurse directiveTreeGraphic/* -ErrorAction SilentlyContinue
+Remove-Item -Recurse crawler-record/* -ErrorAction SilentlyContinue
